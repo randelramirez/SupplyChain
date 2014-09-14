14e47d8583c71f0a070ee923dd1948b91c86c510
Randel Ramirez <randel1_23@hotmail.com>
2014-09-14 19:59:39 +0800

    commit

---
+++ SupplyChain.Core/Models/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplyChain.Core.Models
+{
+    public interface IRepository
+    {
+        IQueryable<T> GetAll<T>() where T: class, new();
+
+        T GetSingle<T>(Func<T, bool> predicate) where T: class, new();
+
+        T Find<T>(int? id) where T : class, new();
+
+        void Add<T>(T entity) where T: class, new();
+
+        void Update<T>(T entity) where T: class, new();
+
+        void Delete<T>(T entity) where T: class, new();
+
+        int Save();
+    }
+}
